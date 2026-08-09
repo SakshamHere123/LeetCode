26afd86eae5217e15427a7211cfc23d235a0dbc0
Saksham Shrivastava <145796142+SakshamHere123@users.noreply.github.com>
2026-08-09 17:19:32 +0530

    Time: 11 ms (35.21%), Space: 22.9 MB (86.27%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    int findMax(vector<int> &v){
+        int n = v.size();
+        int maxi = INT_MIN;
+        for(int i = 0; i< n; i++){
+            maxi = max(maxi,v[i]);
+        }
+        return maxi;
+    }
+
+    long long calcTotalHrs(vector<int> &v, int h){
+        long long totalHrs = 0;
+        int n = v.size();
+        for(int i = 0; i< n;i++){
+            totalHrs += ceil((double)(v[i])/(double)(h));
+        }
+        return totalHrs;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = findMax(piles);
+        while(low<=high){
+            int mid = (low + high)/2;
+            long long totalH = calcTotalHrs(piles,mid);
+            if(totalH <= h){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
