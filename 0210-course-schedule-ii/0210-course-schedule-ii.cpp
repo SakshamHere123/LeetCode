56b2fc51d9a3b7edda6d3a7cd0f41052ef5963f1
Saksham Shrivastava <145796142+SakshamHere123@users.noreply.github.com>
2026-09-24 13:04:57 +0530

    Time: 7 ms (34.77%), Space: 18.7 MB (43.54%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool dfs(int src,vector<int> adj[], vector<int> &vis,vector<int> & pv, vector<int>& path){
+        vis[src]= 1;
+        pv[src]=1;
+        
+        for(auto it : adj[src]){
+            if(!vis[it]){
+                if(dfs(it,adj,vis,pv,path)==true) return true;
+            }
+            else if(pv[it]){
+                return true;
+            }
+        }
+        pv[src]=0;
+        path.push_back(src);
+        return false;
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> path;
+        
+        vector<int> vis(numCourses,0);
+        vector<int> pv(numCourses,0);
+        for(int i = 0; i< numCourses;i++){
+            if(!vis[i]){
+                if(dfs(i,adj,vis,pv,path)==true) return {};
+            }
+        }
+        reverse(path.begin(),path.end());
+        return path;
+    }
+};
